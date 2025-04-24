ef14c43274034b7c677945cf0eb8a62c92506dad
Kavi Dey <kaviasher@gmail.com>
2025-04-24 01:43:59 -0700

    Small rounding bug fix

--- examples/exercises/fma16/fmaround.sv
+++ examples/exercises/fma16/fmaround.sv
@@ -23,23 +23,20 @@ module fmaround (
     output logic             round_overflow,
     output logic [4:0]       round_flags
 );
+
+    ///// 8. Round the result and handle special cases: R = round(M) /////
+    assign round_overflow = (m_exp > `EMAX);
+
     // roundmode:
     //      00: round to zero
     //      01: round to even
     //      10: round down (toward negative infinity)
     //      11: round up (toward positive infinity)
-
-    // 01 00 11 10
-
-
-    ///// 8. Round the result and handle special cases: R = round(M) /////
-    assign round_overflow = (m_exp > `EMAX);
-
     logic rne, rz, rp, rn;
-    assign rne = roundmode == 2'b01;
-    assign rz = roundmode == 2'b00;
-    assign rp = roundmode == 2'b11;
-    assign rn = roundmode == 2'b10;
+    assign rz = roundmode == 2'b00; // round to zero
+    assign rne = roundmode == 2'b01; // round to even
+    assign rp = roundmode == 2'b10; // round up (toward positive infinity)
+    assign rn = roundmode == 2'b11; // round down (toward negative infinity)
 
     // sign_overflow_L_G_sticky
     assign round_flags = {m_sign, round_overflow, m_fract[0], m_shifted[2*`NF+1], |m_shifted[2*`NF:0]};
@@ -50,28 +47,24 @@ module fmaround (
     always_comb begin
         casez (round_flags)
             5'b?_0_?_0_0: round_op = TRUNC;
-            5'b?_0_?_0_1:
+            5'b0_0_?_0_1:
                 if (rp)
                     round_op = RND;
                 else
                     round_op = TRUNC;
-            5'b?_0_0_1_0:
+            5'b0_0_0_1_0:
                 if (rp)
                     round_op = RND;
                 else
                     round_op = TRUNC;
-            5'b?_0_1_1_0:
+            5'b0_0_1_1_0:
                 if (rne | rp)
                     round_op = RND;
                 else
                     round_op = TRUNC;
-            5'b?_0_?_1_1:
+            5'b0_0_?_1_1:
                 if (rne | rp)
-                    round_op = RND;
-                    // if (a_sticky & diff_sign) // could also use (kill_prod | kill_z) instead of a_sticky
-                    //     round_op = TRUNC;
-                    // else
-                    //     round_op = RND;
+                    round_op = RND; 
                 else
                     round_op = TRUNC;
             5'b0_1_?_?_?:
@@ -79,6 +72,26 @@ module fmaround (
                     round_op = P_MAXNUM;
                 else
                     round_op = P_INF;
+            5'b1_0_?_0_1:
+                if (rn)
+                    round_op = RND;
+                else
+                    round_op = TRUNC;
+            5'b1_0_0_1_0:
+                if (rn)
+                    round_op = RND;
+                else
+                    round_op = TRUNC;
+            5'b1_0_1_1_0:
+                if (rne | rn)
+                    round_op = RND;
+                else
+                    round_op = TRUNC;
+            5'b1_0_?_1_1:
+                if (rne | rn)
+                    round_op = RND; 
+                else
+                    round_op = TRUNC;
             5'b1_1_?_?_?:
                 if (rz | rp)
                     round_op = N_MAXNUM;
